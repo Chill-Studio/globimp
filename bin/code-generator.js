140a9c9fd2a6a73a6250a9fac298a3d364b9aa50
rlebhar <robin.lebhar.chill.studio@gmail.com>
2022-05-09 23:16:25 +0200

    fix(build): missing build script

--- bin/code-generator.js
+++ bin/code-generator.js
@@ -33,37 +33,41 @@ function generateGlobalImportsCode() {
     var _loop_1 = function (importKey) {
         var _a;
         //handle default imports
+        statementList[importKey] = { imports: [], typings: [], globalVars: [] };
         if (currentConfig.defaultImports[importKey] === true) {
             statementList = __assign(__assign({}, statementList), (_a = {}, _a[importKey] = {
-                imports: ["import * as ".concat(importKey, "_ from \"").concat(importKey, "\"\n")],
-                typings: ["\n    var ".concat(importKey, " : typeof ").concat(importKey, "_.default\n")],
-                globalVars: ["global.".concat(importKey, " = ").concat(importKey, "_ as any\n")]
+                imports: ["import * as ".concat(importKey, "_ from \"").concat(importKey, "\"")],
+                typings: ["    var ".concat(importKey, " : typeof ").concat(importKey, "_.default")],
+                globalVars: ["global.".concat(importKey, " = ").concat(importKey, "_ as any")]
             }, _a));
         }
         else {
-            // Build an object having a CodeStatements shape to easily create a string out of it
-            var currentNamedImportList = currentConfig.namedImports[importKey];
-            statementList[importKey] = (currentNamedImportList.reduce(function (acc, namedImport) {
-                var _a, _b, _c;
-                acc[importKey] = {
-                    imports: __spreadArray(__spreadArray([], (((_a = acc[importKey]) === null || _a === void 0 ? void 0 : _a.imports) || []), true), ["".concat(namedImport, " as ").concat(namedImport, "_")], false),
-                    typings: __spreadArray(__spreadArray([], (((_b = acc[importKey]) === null || _b === void 0 ? void 0 : _b.typings) || []), true), ["    var ".concat(namedImport, " : typeof ").concat(namedImport, "_")], false),
-                    globalVars: __spreadArray(__spreadArray([], (((_c = acc[importKey]) === null || _c === void 0 ? void 0 : _c.globalVars) || []), true), ["global.".concat(namedImport, " = ").concat(namedImport, "_ as any")], false)
-                };
-                return acc;
-            }, {}))[importKey];
-            statementList[importKey].imports = ["import { ".concat(statementList[importKey].imports.join(", "), " } from \"").concat(importKey, "\"\n")];
+            if (currentConfig.namedImports[importKey].length > 0) {
+                // Build an object having a CodeStatements shape to easily create a string out of it
+                var currentNamedImportList = currentConfig.namedImports[importKey];
+                statementList[importKey] = (currentNamedImportList.reduce(function (acc, namedImport) {
+                    var _a, _b, _c;
+                    acc[importKey] = {
+                        imports: __spreadArray(__spreadArray([], (((_a = acc[importKey]) === null || _a === void 0 ? void 0 : _a.imports) || []), true), ["".concat(namedImport, " as ").concat(namedImport, "_")], false),
+                        typings: __spreadArray(__spreadArray([], (((_b = acc[importKey]) === null || _b === void 0 ? void 0 : _b.typings) || []), true), ["    var ".concat(namedImport, " : typeof ").concat(namedImport, "_")], false),
+                        globalVars: __spreadArray(__spreadArray([], (((_c = acc[importKey]) === null || _c === void 0 ? void 0 : _c.globalVars) || []), true), ["global.".concat(namedImport, " = ").concat(namedImport, "_ as any")], false)
+                    };
+                    return acc;
+                }, {}))[importKey];
+                statementList[importKey].imports = ["import { ".concat(statementList[importKey].imports.join(", "), " } from \"").concat(importKey, "\"")];
+            }
         }
-        importGeneratedCode += statementList[importKey].imports;
-        typingsGeneratedCode += "".concat(statementList[importKey].typings.join("\n"));
+        importGeneratedCode += statementList[importKey].imports + "\n";
+        typingsGeneratedCode += "".concat(statementList[importKey].typings.join("\n"), "\n");
         globalVarsGeneratedCode += "".concat(statementList[importKey].globalVars.join("\n"), "\n");
     };
     for (var importKey in currentConfig.namedImports) {
         _loop_1(importKey);
     }
-    if (typingsGeneratedCode !== "") {
-        typingsGeneratedCode = "\ndeclare global {\n".concat(typingsGeneratedCode, "}\n\n");
+    if (typingsGeneratedCode.trim().length > 0) {
+        console.log("La valeur ", typingsGeneratedCode);
+        typingsGeneratedCode = "declare global {\n".concat(typingsGeneratedCode, "}\n");
     }
-    (0, fs_1.writeFileSync)(currentDirectory + "/src/globimp.ts", "/* Auto-generated file using globimp */\n\n     ".concat(importGeneratedCode, "\n     ").concat(typingsGeneratedCode, "\n     ").concat(globalVarsGeneratedCode));
+    (0, fs_1.writeFileSync)(currentDirectory + "/src/globimp.ts", "/* Auto-generated file using globimp */\n".concat(importGeneratedCode, "\n").concat(typingsGeneratedCode, "\n").concat(globalVarsGeneratedCode));
 }
 exports.generateGlobalImportsCode = generateGlobalImportsCode;
--- bin/code-generator.ts
+++ bin/code-generator.ts
@@ -12,42 +12,53 @@ export function generateGlobalImportsCode() {
     let statementList: { [key: string]: CodeStatements } = {}
     for (let importKey in currentConfig.namedImports) {
         //handle default imports
+        statementList[importKey] = { imports: [], typings: [], globalVars: [] }
         if (currentConfig.defaultImports[importKey] === true) {
+
             statementList = {
                 ...statementList,
                 [importKey]: {
-                    imports: [`import * as ${importKey}_ from "${importKey}"\n`],
-                    typings: [`\n    var ${importKey} : typeof ${importKey}_.default\n`],
-                    globalVars: [`global.${importKey} = ${importKey}_ as any\n`]
+                    imports: [`import * as ${importKey}_ from "${importKey}"`],
+                    typings: [`    var ${importKey} : typeof ${importKey}_.default`],
+                    globalVars: [`global.${importKey} = ${importKey}_ as any`]
                 },
 
             }
+
         } else {
-            // Build an object having a CodeStatements shape to easily create a string out of it
-            const currentNamedImportList = currentConfig.namedImports[importKey];
-            statementList[importKey] = (currentNamedImportList.reduce((acc, namedImport) => {
-                acc[importKey] = {
-                    imports: [...(acc[importKey]?.imports || []), `${namedImport} as ${namedImport}_`],
-                    typings: [...(acc[importKey]?.typings || []), `    var ${namedImport} : typeof ${namedImport}_`],
-                    globalVars: [...(acc[importKey]?.globalVars || []), `global.${namedImport} = ${namedImport}_ as any`],
+            if (currentConfig.namedImports[importKey].length > 0) {
+                // Build an object having a CodeStatements shape to easily create a string out of it
+                const currentNamedImportList = currentConfig.namedImports[importKey];
+                statementList[importKey] = (currentNamedImportList.reduce((acc, namedImport) => {
+                    acc[importKey] = {
+                        imports: [...(acc[importKey]?.imports || []), `${namedImport} as ${namedImport}_`],
+                        typings: [...(acc[importKey]?.typings || []), `    var ${namedImport} : typeof ${namedImport}_`],
+                        globalVars: [...(acc[importKey]?.globalVars || []), `global.${namedImport} = ${namedImport}_ as any`]
 
-                }
-                return acc
+                    }
+                    return acc
 
-            }, {}))[importKey] as CodeStatements
-            statementList[importKey].imports = [`import { ${statementList[importKey].imports.join(", ")} } from "${importKey}"\n`]
+                }, {}))[importKey] as CodeStatements
+
+                statementList[importKey].imports = [`import { ${statementList[importKey].imports.join(", ")} } from "${importKey}"`]
+
+            }
         }
-        importGeneratedCode += statementList[importKey].imports
-        typingsGeneratedCode += `${statementList[importKey].typings.join("\n")}`
+        importGeneratedCode += statementList[importKey].imports + "\n"
+        typingsGeneratedCode += `${statementList[importKey].typings.join("\n")}\n`
         globalVarsGeneratedCode += `${statementList[importKey].globalVars.join("\n")}\n`
+
+
     }
-    if (typingsGeneratedCode !== "") {
-        typingsGeneratedCode = `\ndeclare global {\n${typingsGeneratedCode}}\n\n`
+    if (typingsGeneratedCode.trim().length > 0) {
+        console.log("La valeur ", typingsGeneratedCode)
+
+        typingsGeneratedCode = `declare global {\n${typingsGeneratedCode}}\n`
     }
-    writeFileSync(currentDirectory + `/src/globimp.ts`, `/* Auto-generated file using globimp */\n
-     ${importGeneratedCode}
-     ${typingsGeneratedCode}
-     ${globalVarsGeneratedCode}`);
+    writeFileSync(currentDirectory + `/src/globimp.ts`, `/* Auto-generated file using globimp */
+${importGeneratedCode}
+${typingsGeneratedCode}
+${globalVarsGeneratedCode}`);
 
 }
 
