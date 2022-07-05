dca636423e0658974611b53880c3367fbe497c63
Robin Lebhar <robin_lebhar@hotmail.fr>
2022-07-05 16:29:26 +0200

    fix

--- bin/code-generator.js
+++ bin/code-generator.js
@@ -1,6 +1,6 @@
 "use strict";
 var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function (t) {
+    __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
             s = arguments[i];
             for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
--- src/globimp.ts
+++ src/globimp.ts
@@ -1,17 +1,9 @@
 /* eslint-disable no-var */
 /* Auto-generated file using globimp */
-import * as chakra_ui_react_ from "@chakra-ui/react";
-import * as axios_ from "axios";
-import { useState as useState_, useEffect as useEffect_ } from "react";
+import { a as a_ } from "typescript"
 
 declare global {
-  var chakra_ui_react: typeof chakra_ui_react_;
-  var axios: typeof axios_.default;
-  var useState: typeof useState_;
-  var useEffect: typeof useEffect_;
+    var a : typeof a_
 }
 
-window.chakra_ui_react = chakra_ui_react_ as any;
-window.axios = axios_ as any;
-window.useState = useState_ as any;
-window.useEffect = useEffect_ as any;
+window.a = a_ as any
